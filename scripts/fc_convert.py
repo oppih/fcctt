77bee38a8273f199a1b52aeda0dc42a73120e000
oppih-374 <letwego28@gmail.com>
2012-01-02 18:21:51 +0800

    replce the file path space with '_'

--- scripts/fc_convert.py
+++ scripts/fc_convert.py
@@ -12,13 +12,15 @@ pattern1 = '*.png'
 def walkThrough():
     for root, dirs, files in os.walk(rootPath):
         for filename in fnmatch.filter(files, pattern0):
-            print os.path.join(root, filename) # this produces a string
+            print os.path.join(root, filename).replace(' ', '_') # this produces a string
         for filename in fnmatch.filter(files, pattern1):
             #print os.path.join(root, filename)
-            convertPic(os.path.join(root, filename))
+            #convertPic(os.path.join(root, filename))
+            print os.path.join(root, filename).replace(' ', '_') # this produces a string
 
 def convertPic(picPathStr):
     # use ImageMagic tool to convert the size and file pattern
+    pass
 
 
 if __name__ == "__main__":
